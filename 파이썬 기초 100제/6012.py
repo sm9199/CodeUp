576338c64f611071b19d2cb16c2dec0fbe9d17c9
LEE SEUNG MIN <sm000406@naver.com>
2024-11-04 13:06:49 +0900

    Add files via upload
    "2024-11-04"

---
+++ 파이썬 기초 100제/6012.py
@@ -0,0 +1,19 @@
+﻿# 6012 : [기초-입출력] 정수 2개 입력받아 그대로 출력하기1(설명)(py)
+
+# 줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+a = int(input())
+b = int(input())
+
+print(a)
+print(b)
+
+
+# 다른 방법
+a = input()
+b = input()
+a = int(a)
+b = int(b)
+
+print(a)
+print(b)
---
+++ 파이썬 기초 100제/6014.py
@@ -0,0 +1,10 @@
+﻿# 6014 : [기초-입출력] 실수 1개 입력받아 3번 출력하기(py)
+
+# 실수(real number) 1개를 입력받아 줄을 바꿔 3번 출력해보자.
+
+a = float(input())
+
+
+print(a)
+print(a)
+print(a)
